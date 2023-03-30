799f5c4369fbc28f5ec0b744819b15153c298f68
Emmanuel Mokel <manny.mokel@gmail.com>
2023-03-30 16:50:47 -0400

    Added arguments for SWAG training from DistilBERT

--- DistilBERT/DistilBERT-SI.py
+++ DistilBERT/DistilBERT-SI.py
@@ -1,5 +1,62 @@
 # Python file in order to test subspace inference techniques with DistilBERT
 import torch
+import os, sys
 import numpy as np
+import argparse
+import torch.nn.functional as F
+
+# Change directory here to be able to access models properly
+import models
 
 dBERT = torch.load('DistilBERT-Results/pytorch_model.bin')
+
+parser = argparse.ArgumentParser(description='SGD/SWA training')
+parser.add_argument('--dir', type=str, default=None, required=True, help='training directory (default: None)')
+parser.add_argument('--use_test', dest='use_test', action='store_true', help='use test dataset instead of validation (default: False)')
+parser.add_argument('--split_classes', type=int, default=None)
+parser.add_argument('--batch_size', type=int, default=128, metavar='N', help='input batch size (default: 128)')
+parser.add_argument('--num_workers', type=int, default=4, metavar='N', help='number of workers (default: 4)')
+parser.add_argument('--model', type=str, default=None, required=True, metavar='MODEL',
+                    help='model name (default: None)')
+parser.add_argument('--resume', type=str, default=None, metavar='CKPT',
+                    help='checkpoint to resume training from (default: None)')
+parser.add_argument('--epochs', type=int, default=200, metavar='N', help='number of epochs to train (default: 200)')
+parser.add_argument('--save_freq', type=int, default=25, metavar='N', help='save frequency (default: 25)')
+parser.add_argument('--eval_freq', type=int, default=5, metavar='N', help='evaluation frequency (default: 5)')
+parser.add_argument('--lr_init', type=float, default=0.01, metavar='LR', help='initial learning rate (default: 0.01)')
+parser.add_argument('--momentum', type=float, default=0.9, metavar='M', help='SGD momentum (default: 0.9)')
+parser.add_argument('--wd', type=float, default=1e-4, help='weight decay (default: 1e-4)')
+parser.add_argument('--swag', action='store_true')
+parser.add_argument('--swag_start', type=float, default=161, metavar='N', help='SWA start epoch number (default: 161)')
+parser.add_argument('--swag_lr', type=float, default=0.02, metavar='LR', help='SWA LR (default: 0.02)')
+parser.add_argument('--swag_c_epochs', type=int, default=1, metavar='N',
+                    help='SWA model collection frequency/cycle length in epochs (default: 1)')
+parser.add_argument('--cov_mat', action='store_true', help='save sample covariance')
+parser.add_argument('--max_num_models', type=int, default=20, help='maximum number of SWAG models to save')
+parser.add_argument('--swag_resume', type=str, default=None, metavar='CKPT',
+                    help='checkpoint to restor SWA from (default: None)')
+parser.add_argument('--loss', type=str, default='CE', help='loss to use for training model (default: Cross-entropy)')
+parser.add_argument('--seed', type=int, default=1, metavar='S', help='random seed (default: 1)')
+parser.add_argument('--no_schedule', action='store_true', help='store schedule')
+parser.add_argument('--save_iterates', action='store_true', help='save all iterates in the SWA(G) stage (default: off)')
+parser.add_argument('--inference', choices=['low_rank_gaussian', 'projected_sgd'], default='low_rank_gaussian')
+parser.add_argument('--subspace', choices=['covariance', 'pca', 'freq_dir'], default='covariance')
+args = parser.parse_args()
+
+args.device = None
+if torch.cude.is_available():
+    args.device = torch.device('cuda')
+else:
+    args.device = torch.device('cpu')
+
+print('Preparing directory %s' % args.dir)
+os.makedirs(args.dir, exist_ok=True)
+with open(os.path.join(args.dir, 'command.sh'), 'w') as f:
+    f.write(' '.join(sys.argv))
+    f.write('\n')
+
+torch.backends.cudnn.benchmark = True
+torch.manual_seed(args.seed)
+torch.cuda.manual_seed(args.seed)
+
+print()
