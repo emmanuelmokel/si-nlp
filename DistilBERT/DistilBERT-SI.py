82b30c45600d8b78315113c7caaf0bccdf947745
Emmanuel Mokel <manny.mokel@gmail.com>
2023-04-07 09:38:36 -0400

    Fixed typo in device

--- DistilBERT/DistilBERT-SI.py
+++ DistilBERT/DistilBERT-SI.py
@@ -91,7 +91,7 @@ else:
 
 # Handle the repository creation
 args.device = None
-if torch.cude.is_available():
+if torch.cuda.is_available():
     args.device = torch.device('cuda')
 else:
     args.device = torch.device('cpu')
